fb5cf4ff226d022906d6eeb87026c46d1c7c2b53
tayyib goolamallee <tayyibgoolamallee@gmail.com>
2026-01-25 16:11:16 +0000

    Improve Checkbox component: handle checked prop and onChange correctly

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,16 +1,18 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface CheckboxProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
   onCheckedChange?: (checked: boolean) => void
+  checked?: boolean
 }
 
 const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
-  ({ className, onCheckedChange, ...props }, ref) => {
+  ({ className, onCheckedChange, checked, ...props }, ref) => {
     return (
       <input
         type="checkbox"
         ref={ref}
+        checked={checked}
         className={cn(
           "h-4 w-4 rounded border-input text-primary focus:ring-2 focus:ring-primary focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
